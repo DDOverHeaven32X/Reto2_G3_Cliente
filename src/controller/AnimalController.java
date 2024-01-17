80e96fe32bb98cf76f897fc08391da1b47a10859
AdrianMoreno29 <adrianmosan@gmail.com>
2024-01-17 14:25:15 +0100

    ventana de animal a medias

--- src/controller/AnimalController.java
+++ src/controller/AnimalController.java
@@ -6,7 +6,17 @@
 package controller;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
@@ -19,18 +29,31 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import logic.AnimalFactoria;
+import model.Admin;
+import model.Animal;
+import model.Entrada;
+import model.Usuario;
+import model.Zona;
 
 /**
  * FXML Controller class
  *
- * @author 2dam
+ * @author Adrian
  */
-public class AnimalController{
-    
-    
+public class AnimalController {
+
+    private static final Logger LOGGER = Logger.getLogger("/controller/AnimalController");
+
+    private Admin admin = new Admin();
+    private Usuario user = new Usuario();
+    private final AnimalFactoria fAnimal = new AnimalFactoria();
+    private ObservableList<Animal> animalData;
 
     @FXML
     private Pane paneAnimal;
@@ -45,7 +68,7 @@ public class AnimalController{
     @FXML
     private TextField txtNombreAnimal;
     @FXML
-    private ComboBox<?> txtGenero;
+    private ComboBox txtGenero;
     @FXML
     private TextField txtEdad;
     @FXML
@@ -53,60 +76,221 @@ public class AnimalController{
     @FXML
     private TextField txtAltura;
     @FXML
-    private ComboBox<?> comboSalud;
+    private ComboBox comboSalud;
     @FXML
-    private ComboBox<?> comboAlimentacion;
+    private ComboBox comboAlimentacion;
     @FXML
     private TextField txtEspecie;
     @FXML
-    private ComboBox<?> comboZona;
+    private ComboBox comboZona;
     @FXML
-    private TableView<?> tableAnimal;
+    private TableView<Animal> tableAnimal;
     @FXML
-    private TableColumn<?, ?> tbcNombre;
+    private TableColumn tbcNombre;
     @FXML
-    private TableColumn<?, ?> tbcEspecie;
+    private TableColumn tbcEspecie;
     @FXML
-    private TableColumn<?, ?> tbcGenero;
+    private TableColumn tbcGenero;
     @FXML
-    private TableColumn<?, ?> tbcEdad;
+    private TableColumn tbcEdad;
     @FXML
-    private TableColumn<?, ?> tbcPeso;
+    private TableColumn tbcPeso;
     @FXML
-    private TableColumn<?, ?> tbcAltura;
+    private TableColumn tbcAltura;
     @FXML
-    private TableColumn<?, ?> tbcAlimentacion;
+    private TableColumn tbcAlimentacion;
     @FXML
-    private TableColumn<?, ?> tbcSalud;
+    private TableColumn tbcSalud;
     @FXML
-    private TableColumn<?, ?> tbcZona;
+    private TableColumn <Zona, ?> tbcZona;
     @FXML
-    private ComboBox<?> comboFiltrar;
+    private ComboBox comboFiltrar;
     @FXML
     private Button btnBuscar;
     @FXML
     private Button btnInforme;
     @FXML
-    private ComboBox<?> comboFiltrarAlimentacion;
+    private ComboBox comboFiltrarAlimentacion;
     @FXML
-    private ComboBox<?> comboFiltrarEspecie;
-    
-    
+    private ComboBox comboFiltrarEspecie;
+
     private Stage stage;
 
-    
     public void initiStage(Parent root) {
         Scene scene = new Scene(root);
         Stage stage = new Stage();
         stage.setScene(scene);
+        //La ventana no es modal
+        stage.initModality(Modality.NONE);
         stage.setTitle("Animal");
+        //Campos visibles
+        txtNombreAnimal.setVisible(true);
+        comboFiltrar.setVisible(true);
+        txtGenero.setVisible(true);
+        txtEdad.setVisible(true);
+        txtPeso.setVisible(true);
+        txtAltura.setVisible(true);
+        comboSalud.setVisible(true);
+        comboAlimentacion.setVisible(true);
+        txtEspecie.setVisible(true);
+        comboZona.setVisible(true);
+        //Campos invisibles
+        comboFiltrarAlimentacion.setVisible(false);
+        btnBuscar.setVisible(false);
+        comboFiltrarEspecie.setVisible(false);
+        //Campos deshabilitados
+        btnCrearAimal.setDisable(false);
+        btnModificarAnimal.setDisable(false);
+        btnEliminarAnimal.setDisable(false);
+        comboFiltrarAlimentacion.setDisable(true);
+        btnBuscar.setDisable(true);
+        comboFiltrarEspecie.setDisable(true);
+        //El foco está en el campo del precio
+        txtNombreAnimal.requestFocus();
+        //Activacion del cambio de texto
+        txtNombreAnimal.textProperty().addListener(this::cambioTexto);
+        //Combo con sus datos ya introducidos
+        txtGenero.getItems().addAll("Macho", "Hembra");
+        comboFiltrar.getItems().addAll("Filtrar por alimentación", "Filtrar por especie");
+        comboSalud.getItems().addAll("SANO", "ENFERMO");
+        comboZona.getItems().addAll(fAnimal.getFactory());
+        cargarFiltroComboEspecie(comboFiltrarEspecie);
+        comboFiltrarAlimentacion.getItems().addAll("HERBIVORO", "CARNIVORO", "OMNIVORO");
+        //Los datos de la fila selecionada se añadirán a los campos con esto
+        tableAnimal.getSelectionModel().selectedItemProperty().addListener(this::handleUsersTableSelectionChanged);
+        //Asignacion de botones
+        /*btnCrearAimal.setOnAction(this::handleCreateButtonAction);
+        btnModificarAnimal.setOnAction(this::handleModifyButtonAction);
+        btnEliminarAnimal.setOnAction(this::handleDeleteButtonAction);*/
+        //Dependiendo que tipo de filtro se escoja, ciertos elementos de la ventana se alteran
+        comboFiltrar.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                if (null == comboFiltrar.getValue().toString()) {
+                    comboFiltrarAlimentacion.setDisable(true);
+                    comboFiltrarEspecie.setDisable(true);
+                    btnBuscar.setVisible(false);
+                } else {
+                    switch (comboFiltrar.getValue().toString()) {
+                        case "Filtrar por alimentación":
+                            comboFiltrarAlimentacion.setDisable(false);
+                            comboFiltrarAlimentacion.setVisible(true);
+                            comboFiltrarEspecie.setDisable(true);
+                            comboFiltrarEspecie.setVisible(false);
+                            btnBuscar.setVisible(true);
+                            btnBuscar.setDisable(false);
+                            break;
+                        case "Filtrar por especie":
+                            comboFiltrarEspecie.setDisable(false);
+                            comboFiltrarEspecie.setVisible(true);
+                            comboFiltrarAlimentacion.setDisable(true);
+                            comboFiltrarAlimentacion.setVisible(false);
+                            btnBuscar.setVisible(true);
+                            btnBuscar.setDisable(false);
+                            break;
+                        default:
+                            comboFiltrarAlimentacion.setDisable(true);
+                            comboFiltrarAlimentacion.setVisible(false);
+                            comboFiltrarEspecie.setDisable(true);
+                            comboFiltrarEspecie.setVisible(false);
+                            btnBuscar.setVisible(true);
+                            btnBuscar.setDisable(false);
+                            cargarTodo();
+                            break;
+                    }
+                }
+            }
+        });
+
+        //Establecemos las factorias para los valores de celda
+        try {
+            tbcNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+            tbcEspecie.setCellValueFactory(new PropertyValueFactory<>("especie"));
+            tbcGenero.setCellValueFactory(new PropertyValueFactory<>("genero"));
+            tbcEdad.setCellValueFactory(new PropertyValueFactory<>("edad"));
+            tbcPeso.setCellValueFactory(new PropertyValueFactory<>("peso"));
+            tbcAltura.setCellValueFactory(new PropertyValueFactory<>("altura"));
+            tbcAlimentacion.setCellValueFactory(new PropertyValueFactory<>("alimentacion"));
+            tbcSalud.setCellValueFactory(new PropertyValueFactory<>("salud"));
+            tbcZona.setCellValueFactory(new PropertyValueFactory<>("zona"));
+
+            //cargamos los datos en la tabla
+            animalData = FXCollections.observableArrayList(fAnimal.getFactory().findAll_XML(Animal.class));
+            tableAnimal.setItems(animalData);
+        } catch (Exception e) {
+            LOGGER.severe("Error a la hora de cargar los datos");
+        }
+
         stage.show();
-    }    
+    }
+
+    /**
+     * Método que frente a cualquier cambio de la tabla la pone en estado
+     * neutral, vacia los campos y muestra los datos de la tabla.
+     */
+    @FXML
+
+    private ObservableList<Animal> cargarTodo() {
+        ObservableList<Animal> listAnimales;
+        List<Animal> todosAnimales;
+        todosAnimales = FXCollections.observableArrayList(fAnimal.getFactory().findAll_XML(Animal.class));
+        listAnimales = FXCollections.observableArrayList(todosAnimales);
+        txtNombreAnimal.setText("");
+        txtGenero.setValue(null);
+        txtEspecie.setText("");
+        comboSalud.setValue(null);
+        txtEdad.setText("");
+        txtPeso.setText("");
+        txtAltura.setText("");
+        comboAlimentacion.setValue(null);
+        comboZona.setValue(null);
+        tableAnimal.setItems(listAnimales);
+        tableAnimal.refresh();
+        return listAnimales;
+    }
+
+    //Método que vacia los campos si hay algúna alteracion en la ventana
+    @FXML
+    private void cambioTexto(ObservableValue observable, Object oldValue, Object newValue) {
+        if (txtNombreAnimal.getText().trim().isEmpty() || txtGenero.getValue().toString().trim().isEmpty() || txtEspecie.getText().trim().isEmpty() || comboSalud.getValue().toString().trim().isEmpty() || txtEdad.getText().trim().isEmpty() || txtPeso.getText().trim().isEmpty() || txtAltura.getText().trim().isEmpty() || comboAlimentacion.getValue().toString().trim().isEmpty() || comboZona.getValue().toString().trim().isEmpty()) {
+            btnCrearAimal.setDisable(true);
+            btnModificarAnimal.setDisable(true);
+            btnEliminarAnimal.setDisable(true);
+        } else {
+            btnCrearAimal.setDisable(false);
+            btnModificarAnimal.setDisable(false);
+            btnEliminarAnimal.setDisable(false);
+        }
+    }
+
+    //Método que introduce los datos de la tabla aal panel
+    @FXML
+    private void handleUsersTableSelectionChanged(ObservableValue observable, Object oldValue, Object newValue) {
+
+        if (newValue != null) {
+
+            Animal animal = (Animal) newValue;
+
+            txtNombreAnimal.setText(animal.getNombre());
+            txtGenero.setValue(animal.getGenero());
+            txtEspecie.setText(animal.getEspecie());
+            comboSalud.setValue(animal.getSalud());
+            txtEdad.setText(animal.getEdad().toString());
+            txtPeso.setText(animal.getPeso().toString());
+            txtAltura.setText(animal.getAltura().toString());
+            comboAlimentacion.setValue(animal.getAlimentacion());
+            comboZona.setValue(animal.getZona().getNombre());
+        }
+    }
+
+    @FXML
+    private void cargarFiltroComboEspecie(ComboBox comboFiltrarEspecie1) {
+        ObservableList<String> especies = FXCollections.observableArrayList(fAnimal.getFactory().findSpecies_XML(String.class));
+        List<String> todasLasEspecies = FXCollections.observableArrayList(especies);
+        comboFiltrarEspecie1.getItems().addAll(todasLasEspecies);
+    }
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
-    
-    
-    
+
 }
---
+++ src/logic/AnimalRESTClient.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.List;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import model.Animal;
+
+/**
+ * Jersey REST client generated for REST resource:AnimalFacadeREST
+ * [entidades.animal]<br>
+ * USAGE:
+ * <pre>
+ *        AnimalRESTClient client = new AnimalRESTClient();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author Adrian
+ */
+public class AnimalRESTClient implements AnimalInterfaz {
+
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/Reto2_G3_Servidor/webresources";
+
+    public AnimalRESTClient() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("entidades.animal");
+    }
+
+    @Override
+    public List<Animal> findAnimalsBySpecies_XML(Class<Animal> responseType, String especie) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("visualizarAnimalesPorEspecie/{0}", new Object[]{especie}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public List<Animal> findAnimalsBySpecies_JSON(Class<Animal> responseType, String especie) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("visualizarAnimalesPorEspecie/{0}", new Object[]{especie}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public void edit_XML(Object requestEntity) throws WebApplicationException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
+    }
+
+    @Override
+    public void edit_JSON(Object requestEntity) throws WebApplicationException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
+    }
+
+    @Override
+    public List<Animal> find_XML(Class<Animal> responseType, String id_animal) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("buscarPorID/{0}", new Object[]{id_animal}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public List<Animal> find_JSON(Class<Animal> responseType, String id_animal) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("buscarPorID/{0}", new Object[]{id_animal}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public void create_XML(Object requestEntity) throws WebApplicationException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
+    }
+
+    @Override
+    public void create_JSON(Object requestEntity) throws WebApplicationException {
+        webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_JSON));
+    }
+
+    @Override
+    public List<Animal> findAnimalsInAnArea_XML(Class<Animal> responseType, String zona) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("visualizarAnimalesDeUnaZona/{0}", new Object[]{zona}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public List<Animal> findAnimalsInAnArea_JSON(Class<Animal> responseType, String zona) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("visualizarAnimalesDeUnaZona/{0}", new Object[]{zona}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public List<Animal> findAll_XML(Class<Animal> responseType) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public List<Animal> findAll_JSON(Class<Animal> responseType) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public void remove(String id_animal) throws WebApplicationException {
+        webTarget.path(java.text.MessageFormat.format("borrar/{0}", new Object[]{id_animal})).request().delete();
+    }
+
+    @Override
+    public List<Animal> findAnimalsByFeeding_XML(Class<Animal> responseType, String alimentacion) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("visualizarAnimalesPorAlimentacion/{0}", new Object[]{alimentacion}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public List<Animal> findAnimalsByFeeding_JSON(Class<Animal> responseType, String alimentacion) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("visualizarAnimalesPorAlimentacion/{0}", new Object[]{alimentacion}));
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(new GenericType<List<Animal>>() {
+        });
+    }
+
+    @Override
+    public List<String> findSpecies_XML(Class<String> responseType) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path("listarEspecies");
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(new GenericType<List<String>>() {
+        });
+    }
+
+    @Override
+    public List<String> findSpecies_JSON(Class<String> responseType) throws WebApplicationException {
+        WebTarget resource = webTarget;
+        resource = resource.path("listarEspecies");
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_JSON).get(new GenericType<List<String>>() {
+        });
+    }
+
+    public void close() {
+        client.close();
+    }
+    
+}
